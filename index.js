aceb92ca8aa770512bc9087bcfeb40fe9d63840c
Matt Draghi <mtd5264@gmail.com>
2022-01-26 17:01:47 -0500

    commit tree 4da079e9a6f1024a2edbabaff0d416d12dbea9e9

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const stringArr = []
+  const revStringArr = []
+  for(const char in word){
+    stringArr.push(word[char])
+    revStringArr.unshift(word[char])
+  }
+  
+  return (stringArr.join() === revStringArr.join())
+  
 }
 
 /* 
   Add your pseudocode here
+  - write function isPalindrome that will receive a string argument
+  - the function should return true if the string reads the same forwards and backwards
 */
 
 /*
   Add written explanation of your solution here
+  - the function should create a new array that takes each letter from the string
+  - create a new array using the contents of the current array
+  - join the arrays to see if they match
 */
 
 // You can run `node index.js` to view these console logs
